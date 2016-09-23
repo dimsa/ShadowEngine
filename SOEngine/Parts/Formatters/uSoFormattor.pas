5e82362eb589b35e2ad8c2c1415d590364299831
dimsa <dimsa87@gmail.com>
2016-09-23 20:57:56 +0700

    Working on TSoFormattor

--- SOEngine/Parts/Formatters/uSoFormattor.pas
+++ SOEngine/Parts/Formatters/uSoFormattor.pas
@@ -3,11 +3,16 @@ unit uSoFormattor;
 interface
 
 uses
-  System.SyncObjs, System.Classes, System.SysUtils,
+  System.SyncObjs, System.Classes, System.SysUtils, System.RegularExpressions,
   uSoBaseOperator, uSoFormatter, uNamedList, uSoObject, uSoContainerTypes, uSoBasePart;
 
 type
   TSoFormatterTemplate = class
+  private
+    FExpression: string;
+  public
+    property Expression: string read FExpression;
+    constructor Create(const AExpression: string);
   end;
 
   TSoFormattor = class(TSoOperator<TSoFormatter>)
@@ -66,13 +71,44 @@ begin
 end;
 
 procedure TSoFormattor.LoadTemplates(const AFileName: string);
+var
+  vReg: TRegEx;
+  vFile: TStringList;
+  vStrs, vDirective: TArray<string>;
+  i: Integer;
 begin
+  vFile := TStringList.Create;
+  vFile.LoadFromFile(AFileName);
+  // Убираем переносы строк
+  vReg := TRegEx.Create('[\r\n\s\t]*');
+  vFile.Text := vReg.Replace(vFile.Text, '');
 
+  // Делим по стилям
+  vReg := TRegEx.Create('}');
+  vStrs := vReg.Split(vFile.Text);
+  // Делим на название стиля и его текст
+  vReg := TRegEx.Create('{');
+  for i := 0 to Length(vStrs) - 1 do
+  begin
+    vDirective := vReg.Split(vStrs[i]);
+    if Length(vDirective) >= 2 then
+    begin
+      FTemplates.Add(vDirective[0], TSoFormatterTemplate.Create(vDirective[1]));
+    end;
+  end;
+  vFile.Free;
 end;
 
 procedure TSoFormattor.OnItemDestroy(ASender: TObject);
 begin
   FList.Delete(TSoFormatter(ASender));
+end;
+
+{ TSoFormatterTemplate }
+
+constructor TSoFormatterTemplate.Create(const AExpression: string);
+begin
+  FExpression := AExpression;
 end;
 
 end.
