da899674d3198b0442a8a7fdd76f4bff0965061f
Dmitriy Sorokin <dimsa87@gmail.com>
2016-10-21 22:51:43 +0700

    Replaced TTasks on TTimer in uSoSound. Some another work on uSoSound

--- SOEngine/Parts/Sound/uSoSound.pas
+++ SOEngine/Parts/Sound/uSoSound.pas
@@ -3,8 +3,8 @@ unit uSoSound;
 interface
 
 uses
-  uSoBasePart, uGameAudioManager, FMX.Media,
-  uSoObject, System.Threading, System.SysUtils;
+  FMX.Media, FMX.Types,
+  System.Threading, System.SysUtils, uSoBasePart, uSoObject;
 
 type
 
@@ -14,16 +14,18 @@ type
     FVolume: Integer;
     FLoaded: Boolean;
     FLoop: Boolean;
-    FLoopTask: ITask;
+    FTimer: TTimer;
     procedure SetVolume(const Value: Integer);
     procedure SetLoop(const Value: Boolean);
-
+    procedure InitPlay;
+    procedure FinishPlay;
  protected
    procedure OnMediaEnded(ASender: TObject);
  public
     procedure Play; virtual;
     procedure Stop; virtual;
     procedure Pause; virtual;
+    procedure Resume; virtual;
     property Volume: Integer read FVolume write SetVolume;
     property Loop: Boolean read FLoop write SetLoop;
     constructor Create(const ASubject: TSoObject; const AFileName: string);
@@ -39,43 +41,56 @@ var
   vTask: ITask;
 begin
   inherited Create(ASubject);
+
   FLoaded := False;
   FLoop := False;
 
   vTask := TTask.Create (procedure ()
   begin
     try
+      if FMedia <> nil then
+      begin
+        FMedia.DisposeOf;
+        FMedia := nil;
+      end;
+
       FMedia := TMediaCodecManager.CreateFromFile(AFileName);
 
-      FLoopTask := TTask.Create (procedure ()
-      begin
-        Sleep(FMedia.Duration - FMedia.CurrentTime);
-        OnMediaEnded(Self);
-      end);
-
+      FTimer := TTimer.Create(nil);
+      FTimer.Enabled := False;
+      FTimer.OnTimer := OnMediaEnded;
       FLoaded := True;
     except
 
     end;
   end);
   vTask.Start;
-
-
-
 end;
 
 destructor TSoSound.Destroy;
 begin
-  FLoopTask := nil;
+  FTimer.Enabled := False;
+  FTimer.Free;
+  FMedia.Free;
   inherited;
+end;
+
+procedure TSoSound.FinishPlay;
+begin
+  FTimer.Enabled := False;
+end;
+
+procedure TSoSound.InitPlay;
+begin
+  FTimer.Interval := (FMedia.Duration - FMedia.CurrentTime) div 10000;
+  FTimer.Enabled := True;
 end;
 
 procedure TSoSound.OnMediaEnded(ASender: TObject);
 begin
   if FLoop then
   begin
-    FMedia.CurrentTime := 0;
-    FMedia.Play;
+    Play;
   end;
 end;
 
@@ -83,17 +98,40 @@ procedure TSoSound.Pause;
 begin
   if not FLoaded then
     Exit;
-  FLoopTask.Cancel;
+
   FMedia.Stop;
+  FinishPlay;
 end;
 
 procedure TSoSound.Play;
 begin
   if not FLoaded then
     Exit;
+  FMedia.CurrentTime := 0;
   FMedia.Play;
-  FLoopTask.Start;
+  InitPlay;
 end;
+
+procedure TSoSound.Resume;
+begin
+  if not FLoaded then
+    Exit;
+
+  FMedia.Play;
+  InitPlay;
+end;
+
+
+procedure TSoSound.Stop;
+begin
+  if not FLoaded then
+    Exit;
+
+  FMedia.Stop;
+  FMedia.CurrentTime := 0;
+  FinishPlay;
+end;
+
 
 procedure TSoSound.SetLoop(const Value: Boolean);
 begin
@@ -108,13 +146,5 @@ begin
   FVolume := Value;
 end;
 
-procedure TSoSound.Stop;
-begin
-  if not FLoaded then
-    Exit;
-  FLoopTask.Cancel;
-  FMedia.Stop;
-  FMedia.CurrentTime := 0;
-end;
-
 end.
+
--- SOEngine/Parts/Sound/uSoundKeeper.pas
+++ SOEngine/Parts/Sound/uSoundKeeper.pas
@@ -3,7 +3,7 @@ unit uSoundKeeper;
 interface
 
 uses
-  System.SysUtils,
+  System.SysUtils, FMX.Media,
   uSoTypes, uSoContainerTypes, uSoBasePart, uSoBaseOperator, uSoSound, uSoObject,
   uSoObjectDefaultProperties;
 
@@ -11,7 +11,10 @@ type
   TSoSoundKeeper = class(TSoOperator<TSoSound>)
   private
     FTemplates: TList<string>;
+    FMediaPlayer: TMediaPlayer;
+    FInited: Boolean;
     procedure OnItemDestroy(ASender: TObject);
+    procedure InitMediaPlayer(const AName: string);
   public
     procedure Add(const AItem: TSoSound; const AName: string = ''); override;
     function AddFromTemplate(const ASubject: TSoObject; const ATemplateName: string; const AName: string = ''): TSoSound; override;
@@ -49,13 +52,23 @@ end;
 constructor TSoSoundKeeper.Create(const ACritical: TCriticalSection);
 begin
   inherited;
+
+  FInited := False;
   FTemplates := TList<string>.Create;
+
+  FMediaPlayer := TMediaPlayer.Create(nil);
 end;
 
 destructor TSoSoundKeeper.Destroy;
 begin
   FTemplates.Free;
+  FMediaPlayer.Free;
   inherited;
+end;
+
+procedure TSoSoundKeeper.InitMediaPlayer(const AName: string);
+begin
+
 end;
 
 procedure TSoSoundKeeper.OnItemDestroy(ASender: TObject);
